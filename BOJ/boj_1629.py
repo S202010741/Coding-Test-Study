98edaf8f0dbb4083b0da1054a3454a48cbe93b35
s202010741 <s202010741@gmail.com>
2026-02-19 23:49:05 +0900

    Fix: BOJ 1629 3차 시도(Optimize Memory & Time)

--- BOJ/boj_1629.py
+++ BOJ/boj_1629.py
@@ -3,20 +3,5 @@ import sys
 ##1. 입력받기
 A,B,C = map(int,sys.stdin.readline().split())
     
-##2. 거듭제곱 계산
-def power(a, b):
-    # 지수가 1일 때
-    if b == 1:
-        return a%c
-    # 지수 나누어 계산(분할정복)
-    temp = power(a, b//2, c)
-
-    # 지수가 짝수일 때
-    if b%2 ==0 :
-        return(temp*temp)%c
-    #지수가 홀수일 때
-    else:
-        return (temp*temp*a)%c
-    
-    ##3. 결과반환
-    print(power(A,B))
+##2. 내장함수 pow() 사용하여 결과 출력하기
+print(pow(A,B,C))
